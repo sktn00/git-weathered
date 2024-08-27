6bf86437da7ae40c8a9df96ef90907e760e5ea49
sktn00 <m4rc0s.tf@gmail.com>
2024-08-26 21:13:51 -0400

    "Initial commit"

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import requests
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = open("api_key.txt", "r").read()
-CITY = "Asuncion"
+
+city = input(str("Select the location desired: "))
 
 
 def kelvin_to_celsius_fahrenheit(kelvin):
@@ -13,7 +14,7 @@ def kelvin_to_celsius_fahrenheit(kelvin):
     return celsius, fahrenheit
 
 
-url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+url = BASE_URL + "appid=" + API_KEY + "&q=" + city
 response = requests.get(url).json()
 
 temp_kelvin = response["main"]["temp"]
@@ -26,14 +27,14 @@ description = response["weather"][0]["description"]
 sunrise_time = dt.datetime.fromtimestamp(response["sys"]["sunrise"], dt.UTC)
 sunset_time = dt.datetime.fromtimestamp(response["sys"]["sunset"], dt.UTC)
 
-print(f"Temperature in {CITY}: {temp_celsius:.2f}ºC or {temp_fahrenheit:.2f}ºF")
+print(f"Temperature in {city}: {temp_celsius:.2f}ºC or {temp_fahrenheit:.2f}ºF")
 print(
-    f"Temperature in {CITY} feels like: "
+    f"Temperature in {city} feels like: "
     f"{feels_like_celsius:.2f}ºC or "
     f"{feels_like_fahrenheit:.2f}ºF"
 )
-print(f"Humidity in {CITY}: {humidity}%")
-print(f"Wind speed in {CITY}: {wind_speed}m/s")
-print(f"General weather in {CITY}: {description}")
-print(f"Sun rises in {CITY} at {sunrise_time} local time")
-print(f"Sun sets in {CITY} at {sunset_time} local time")
+print(f"Humidity in {city}: {humidity}%")
+print(f"Wind speed in {city}: {wind_speed}m/s")
+print(f"General weather in {city}: {description}")
+print(f"Sun rises in {city} at {sunrise_time} local time")
+print(f"Sun sets in {city} at {sunset_time} local time")
